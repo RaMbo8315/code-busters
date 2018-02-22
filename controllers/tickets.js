53b90d9b25fb378a37d05f5ba8016e813b1e5936
RaMbo8315 <henryurena@yahoo.com>
2018-02-21 21:47:14 -0500

    fixed routes

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,23 +1,40 @@
-var db = require("../models/tickets");
+var db = require("../models");
 
 module.exports = {
-   updateTicket:  function (req, res) {
-    db.Tickets.update(req.body, {
-        where: {
-            assigned_to: req.body
-        }
-    }).then(function (dbTicket) {
-        res.json(dbTicket);
-    });
-},
+    updateTicket: function (req, res) {
+        db.Tickets.update(req.body, {
+            where: {
+                assigned_to: req.body
+            }
+        }).then(function (dbTicket) {
+            res.json(dbTicket);
+        });
+    },
 
     createTicket: function (req, res) {
         db.Tickets.create({
-            name: req.body.name,
-            address: req.body.address,
-            phoneNumber: req.body.PhoneNumber,
+            assigned_to: req.body.assigned_to,
             service: req.body.service,
+            comments: req.body.comments,
+            inStatus: re.body.inStatus,
+            complete: req.body.complete
         }).then(function (dbTicket) {
+            res.json(dbTicket);
+        });
+    },
+
+    findTicket: function (req, res) {
+        db.Tickets.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbTicket) {
+            res.json(dbTicket);
+        });
+    },
+
+    allTickets: function (req, res) {
+        db.Tickets.findAll({}).then(function (dbTicket) {
             res.json(dbTicket);
         });
     }
--- routes/api-Routes.js
+++ routes/api-Routes.js
@@ -1,24 +1,15 @@
 var ticketControllers = require("../controllers/tickets");
+var agentControllers = require("../controllers/agents");
 
 module.exports = function (app) {
-    app.get("/api/admin", function (req, res) {
-        db.Tickets.findAll({}).then(function (dbTicket) {
-            res.json(dbTicket);
-        });
-    });
+    app.get("/api/admin", allTickets);
 
-    // need to set up ticket controllerfor create
+    // need to set up ticket controller for create
     app.post("/api/admin", ticketControllers.createTicket);
 
     app.put("/api/admin", ticketControllers.updateTicket);
 
-    app.get("/api/admin", function (req, res) {
-        db.Tickets.findOne({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (dbTicket) {
-            res.json(dbTicket);
-        });
-    });
+    app.get("/api/admin", findTicket);
+
+    app.post("/api/agents", createAgent)
 }
