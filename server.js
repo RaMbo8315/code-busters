6177824aa1babd8849fbd835c6cb2450e9857563
RaMbo8315 <henryurena@yahoo.com>
2018-02-22 19:18:13 -0500

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path")
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -22,8 +22,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+require("./routes/api-Routes.js")(app);
+require("./routes/html-Routes.js")(app);
 
 db.sequelize.sync({force: true}).then(function () {
     app.listen(PORT, function () {
